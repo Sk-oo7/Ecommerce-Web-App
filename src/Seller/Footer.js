63781f16823b06264f6770bb3fbbc1fb18965823
Shubham Khullar <shubham5khullar@gmail.com>
2020-10-05 23:08:54 +0530

    Added working footer to Sell-Aura
    Footer contains a button to switch between Buy-Aura and Sell-Aura

--- src/Seller/Footer.js
+++ src/Seller/Footer.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import "./Footer.css";
+import "../Footer.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Icons from "./Assets/Img/cards-icons.png";
+import Icons from "../Assets/Img/cards-icons.png";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import InstagramIcon from "@material-ui/icons/Instagram";
@@ -12,10 +12,10 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import RoomIcon from "@material-ui/icons/Room";
 import PhoneIcon from "@material-ui/icons/Phone";
 import EmailIcon from "@material-ui/icons/Email";
-import { db } from "./firebase";
+import { db } from "../firebase";
 import { Link } from "react-router-dom";
 import AccountBalanceRoundedIcon from "@material-ui/icons/AccountBalanceRounded";
-import { useStateValue } from "./StateProvider";
+import { useStateValue } from "../StateProvider";
 
 function Footer() {
   const [Email, setEmail] = useState("");
@@ -93,8 +93,8 @@ function Footer() {
               <Row>
                 <AccountBalanceRoundedIcon style={{ marginLeft: "60px" }} />
                 <h7>
-                  {user && <Link to="/seller">Sell on Sell-Aura</Link>}
-                  {!user && <Link to="/seller/SignIn">Sell on Sell-Aura</Link>}
+                  {user && <a href="/">Buy on Buy-Aura</a>}
+                  {!user && <a href="/SignIn">Buy on Buy-Aura</a>}
                 </h7>
               </Row>
             </center>
--- src/Seller/Seller.js
+++ src/Seller/Seller.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./Footer";
 import Header from "./sHeader";
 import SignIn from "./SignIn.js";
 
@@ -10,21 +11,26 @@ function Seller() {
         <Switch>
           <Route path="/seller" exact>
             <Header />
+            <Footer />
           </Route>
           <Route path="/seller/SignIn" exact>
             <SignIn />
           </Route>
           <Route path="/seller/Catalogue" exact>
             <Header />
+            <Footer />
           </Route>
           <Route path="/seller/Profile" exact>
             <Header />
+            <Footer />
           </Route>
           <Route path="/seller/Orders" exact>
             <Header />
+            <Footer />
           </Route>
           <Route path="/seller/Messages" exact>
             <Header />
+            <Footer />
           </Route>
         </Switch>
       </Router>
