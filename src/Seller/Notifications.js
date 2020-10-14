fc9edccaca4ad178a0d4c67f7e03b07e3e458702
Shubham Khullar <shubham5khullar@gmail.com>
2020-10-14 19:43:36 +0530

    Added Sales table and Notifications area
    Seller can see his sales and notifications about orders and account on main page

--- src/Seller/Notifications.js
+++ src/Seller/Notifications.js
@@ -1,15 +1,33 @@
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Alert } from 'react-bootstrap'
+import { db } from '../firebase'
+import { useStateValue } from '../StateProvider';
 
 function Notifications() {
+    const [note,setNote]=useState([]);
+
+
+    const[{user}]=useStateValue();
+    useEffect(()=>{
+        if(user){
+            db.collection("sellers").doc(user?.uid).collection("notifications").orderBy("created").limit(4).onSnapshot((snapshot) =>
+            setNote(
+            snapshot.docs.map((doc) => {
+                return doc.data()
+            }))
+          );
+        }
+    },[user])
+    console.log("f",note)
+    
     return (
         <div style={{margin:"0 20px 0 0",backgroundColor:"white",width:"30%"}}>
             <h3 style={{color:"gray",textAlign:"center",marginTop:"10px"}}>Notifications</h3>
             <hr/>
             <div style={{marginLeft:"10px",marginRight:"10px"}}>
-            <Alert variant="primary">You Got a New Order</Alert>
-            <Alert variant="success">Money added to your Bank Account</Alert>
+            {note.map((n)=> 
+                    <Alert variant={n?.variant}>{n?.msg}</Alert>
+              )} 
             </div>
             
         </div>
